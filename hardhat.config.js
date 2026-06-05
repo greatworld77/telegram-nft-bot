46d62a547ec01d4c458bfd3be247cf1dcced626b
greatworld77 <greattrust006@gmail.com>
2026-06-05 14:49:24 +0530

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+import "@nomicfoundation/hardhat-ethers";
+
+export default {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
